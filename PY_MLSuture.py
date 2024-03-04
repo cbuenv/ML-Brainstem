440eb5ac5fd823055ff69720494ccc6ae11483e2
Colin Buenvenida <159477401+cbuenv@users.noreply.github.com>
2024-03-04 15:16:36 -0600

    code revamp

--- PY_MLSuture.py
+++ PY_MLSuture.py
@@ -56,13 +56,36 @@ def suture(filepath, column_threshold, row_threshold):
     # Making sure the new data frame is to the user's liking
     proceed = input("\nWould you like to finalize the new dataframe and save it as a file? (y/n): ")
     if proceed.lower() == 'y':
-        fileOutput_name = input("\nEnter a name for the new file: ")
+        fileOutput_name = input("\nEnter a name for the new file (DO NOT INCLUDE .CSV EXTENSION): ")
         fileOutput = f"{fileOutput_name}.csv"
         print("\nSaving new file under the name " + fileOutput)
         df.to_csv(fileOutput, index=False)
 
     else:
         print("\nTerminating session. Will not save the newly-screened dataframe.")
+
+
+def doubleCheck():
+    """
+    A contingency protocol, asks user to provide input in the form of a file pathway, then loads said file and checks
+    if the file in question contains any NaN values.
+
+    :return: Boolean statement
+    """
+
+    print("\n\n- CONTINGENCY ANALYSIS -")
+    print("This protocol, if initiated, will check if a given data frame contains NaN values")
+    contingency_init = input("\nDo you want to initiate contingency analysis? (y/n): ")
+    if contingency_init.lower() == 'y':
+        print("\n\nsuture() Contingency Protocol now booting...")  # Greeting user and initiating protocol
+        double_check = input("What is the name of the CSV file you would like to check?: ")
+
+        df = pd.read_csv(double_check)  # Reading CSV file
+        has_nan_values = df.isna().any().any()  # Checking for NaN values
+        print(f"Does the CSV file have NaN values? {has_nan_values}")  # Printing the result
+
+    else:
+        print("\nWill not initiate contingency protocol. Bye!")
 
 
 if __name__ == "__main__":
@@ -77,19 +100,5 @@ if __name__ == "__main__":
     # Executing suture()
     suture(filepath, column_threshold, row_threshold)
 
-    # Contingency Protocol #############################
-    ''' NOTE: The below code checks if a given data frame contains NaN values'''
-
-    contingency_init = input("\nDo you want to initiate contingency analysis? (y/n): ")
-    if contingency_init.lower() == 'y':
-        print("\n\nsuture() Contingency Protocol now booting...")  # Greeting user and initiating protocol
-        double_check = input("What is the name of the CSV file you would like to check?: ")
-
-        df = pd.read_csv(double_check)  # Reading CSV file
-        has_nan_values = df.isna().any().any()  # Checking for NaN values
-        print(f"Does the CSV file have NaN values? {has_nan_values}")  # Printing the result
-
-    else:
-        print("\nWill not initiate contingency protocol. Bye!")
-    #####################################################
-
+    # Executing doubleCheck()
+    doubleCheck()
